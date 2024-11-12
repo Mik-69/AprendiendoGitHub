664ff26e77507d6a8f41ab46c3eb9f07e44c1768
Mik-69 <mikhailzatonsky@gmail.com>
2024-11-12 14:34:12 +0100

    Metodo saludar con edad agregado a saludo.java

--- saludo.java
+++ saludo.java
@@ -13,4 +13,24 @@ public class saludo {
 
         sc.close();
     }
+
+    public void saludarConEdad () {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Dime tu nombre: ");
+        String nombre = sc.nextLine();
+
+        System.out.print("¿Cuantos años tienes?: ");
+        int edad = sc.nextInt();
+
+        if (edad < 18) {
+            System.out.println("Hola " + nombre + " eres bastante joven");
+        } else {
+            System.out.println("Hola " + nombre + " espero que tengas un buen día");
+        }
+        
+        sc.close();
+
+    }
 }
